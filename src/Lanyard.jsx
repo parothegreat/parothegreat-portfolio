384e15f1006563874040fed30b5f4c7687321e55
parothegreat <alvaroprayogo38@gmail.com>
2026-03-12 13:18:35 +0700

    [fix] path to lanyard

--- src/Lanyard.jsx
+++ src/Lanyard.jsx
@@ -7,7 +7,7 @@ import { BallCollider, CuboidCollider, Physics, RigidBody, useRopeJoint, useSphe
 import { MeshLineGeometry, MeshLineMaterial } from 'meshline';
 
 import cardGLB from './card.glb';
-import lanyard from 'images/profile/pfp.jpeg';
+import lanyard from './lanyard.png';
 
 import * as THREE from 'three';
 
