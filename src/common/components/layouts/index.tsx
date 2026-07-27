6b2baf73ed10772fa9c87ab91356c330ebc3b91e
parothegreat <alvaroprayogo38@gmail.com>
2026-07-27 22:20:40 +0700

    [add] src/common/components/layouts/index.tsx

---
+++ src/common/components/layouts/index.tsx
@@ -0,0 +1,28 @@
+import { ReactNode } from 'react';
+
+import HeaderSidebar from './header/HeaderSidebar';
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+const Layout = ({ children }: LayoutProps) => {
+  return (
+    <div className='min-h-screen'>
+      <a
+        href='#main-content'
+        className='fixed left-4 top-3 z-[1000] -translate-y-20 rounded-md bg-blue-600 px-4 py-2 text-sm font-medium text-white transition-transform focus:translate-y-0 motion-reduce:transition-none'
+      >
+        Skip to content
+      </a>
+      <div className='mx-auto max-w-[1280px] lg:grid lg:grid-cols-[240px_minmax(0,960px)] lg:gap-10 lg:px-6'>
+        <HeaderSidebar />
+        <main id='main-content' className='min-w-0' tabIndex={-1}>
+          {children}
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
