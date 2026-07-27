f029a40c8e7bebc9aa3025e67b813b954789babb
parothegreat <alvaroprayogo38@gmail.com>
2026-07-27 22:21:04 +0700

    [add] src/pages/_app.tsx

---
+++ src/pages/_app.tsx
@@ -0,0 +1,56 @@
+import AOS from 'aos';
+import type { AppProps } from 'next/app';
+import dynamic from 'next/dynamic';
+import { DefaultSeo } from 'next-seo';
+import { ThemeProvider } from 'next-themes';
+import { useEffect } from 'react';
+
+import 'tailwindcss/tailwind.css';
+import 'aos/dist/aos.css';
+import '@/common/styles/globals.css';
+
+import CommandPalette from '@/common/components/elements/CommandPalette';
+import Layout from '@/common/components/layouts';
+import { CommandPaletteProvider } from '@/common/context/CommandPaletteContext';
+import { firaCode, onestSans } from '@/common/styles/fonts';
+
+import defaultSEOConfig from '../../next-seo.config';
+
+const ProgressBar = dynamic(
+  () => import('src/common/components/elements/ProgressBar'),
+  { ssr: false },
+);
+
+const App = ({ Component, pageProps }: AppProps) => {
+  useEffect(() => {
+    AOS.init({
+      duration: 800,
+      delay: 50,
+    });
+  }, []);
+
+  return (
+    <>
+      <style jsx global>
+        {`
+          html {
+            --firaCode-font: ${firaCode.style.fontFamily};
+            --onestSans-font: ${onestSans.style.fontFamily};
+          }
+        `}
+      </style>
+      <DefaultSeo {...defaultSEOConfig} />
+      <ThemeProvider attribute='class' defaultTheme='dark'>
+        <CommandPaletteProvider>
+          <Layout>
+            <CommandPalette />
+            <ProgressBar />
+            <Component {...pageProps} />
+          </Layout>
+        </CommandPaletteProvider>
+      </ThemeProvider>
+    </>
+  );
+};
+
+export default App;
