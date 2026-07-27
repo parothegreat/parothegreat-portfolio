56ca955ed07a87b4c6fd993e81a943df70acf4b3
parothegreat <alvaroprayogo38@gmail.com>
2026-07-27 22:20:44 +0700

    [add] src/common/constant/education.ts

---
+++ src/common/constant/education.ts
@@ -0,0 +1,14 @@
+import { EducationProps } from '../types/education';
+
+export const EDUCATION: EducationProps[] = [
+  {
+    school: 'Mitra Industri Vocational High School',
+    major: 'Industrial Electronics Engineering',
+    logo: '/images/logos/smk-mitra-industri.png',
+    location: 'Bekasi, Indonesia 🇮🇩',
+    degree: 'Vocational High School',
+    start_year: 2024,
+    end_year: null,
+    link: 'https://smkind-mm2100.sch.id',
+  },
+];
