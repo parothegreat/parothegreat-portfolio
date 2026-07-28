e1ab4723714c6c60a82d281cce2c6fcfc4cc61e2
parothegreat <alvaroprayogo38@gmail.com>
2026-07-28 09:09:21 +0700

    [refactor] src/pages/projects/index.tsx

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -30,7 +30,7 @@ const ProjectsPage: NextPage<ProjectsPageProps> = ({ projects }) => {
   return (
     <>
       <NextSeo title={`${PAGE_TITLE} - Alvaro Prayogo`} />
-      <Container data-aos='fade-up'>
+      <Container>
         <PageHeading title={PAGE_TITLE} description={PAGE_DESCRIPTION} />
         <Projects
           projects={projects.slice(0, visibleProjects)}
