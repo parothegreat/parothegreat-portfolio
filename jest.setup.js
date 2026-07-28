ccfd37f279ab42307826e96d3f63b164da6392d6
parothegreat <alvaroprayogo38@gmail.com>
2026-07-28 09:09:40 +0700

    [test] jest.setup.js

--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,29 @@
 import '@testing-library/jest-dom/extend-expect';
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(),
+    removeListener: jest.fn(),
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
+
+global.IntersectionObserver = class IntersectionObserver {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+};
+
+global.ResizeObserver = class ResizeObserver {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+};
+
+window.scrollTo = jest.fn();
